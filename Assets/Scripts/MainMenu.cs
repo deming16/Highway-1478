02ab9e870588011b9d452a7ad70ec8c42b118657
Lin Deming <neverggyay@gmail.com>
2018-06-27 17:33:30 +0800

    Added Endless Level select
    Can now access normal level by play button
    endless game by endless button
    endless button allows player to choose between levels to play

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public GameObject MainMenuUI;
-    public GameObject EndlessSelectUI;
+    public GameObject[] EndlessSelectUI;
+    private int Index;
 
     public void PlayGame()
     {
@@ -15,8 +16,37 @@ public class MainMenu : MonoBehaviour {
 
     public void EndlessSelect()
     {
+        Index = 0;
         MainMenuUI.SetActive(false);
-        EndlessSelectUI.SetActive(true);
+        EndlessSelectUI[Index].SetActive(true);
     }
 
+    public void EndlessPlay()
+    {
+        SceneManager.LoadScene("Endless_" + EndlessSelectUI[Index].name);
+    }
+
+    public void Back()
+    {
+        EndlessSelectUI[Index].SetActive(false);
+        MainMenuUI.SetActive(true);
+    }
+
+    public void Next()
+    {
+        EndlessSelectUI[Index].SetActive(false);
+        Index = (Index + 1) % EndlessSelectUI.Length;
+        EndlessSelectUI[Index].SetActive(true);
+    }
+
+    public void Prev()
+    {
+        EndlessSelectUI[Index].SetActive(false);
+        if(Index - 1 < 0)
+        {
+            Index = EndlessSelectUI.Length;
+        }
+        Index = (Index - 1) % EndlessSelectUI.Length;
+        EndlessSelectUI[Index].SetActive(true);
+    }
 }
