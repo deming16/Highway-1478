3ca7eadf78b4bfbe51d6f046e69a9d564c0ee0be
Lin Deming <neverggyay@gmail.com>
2018-06-27 15:30:25 +0800

    Added and renamed endless levels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public GameObject MainMenuUI;
+    public GameObject EndlessSelectUI;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void EndlessSelect()
+    {
+        MainMenuUI.SetActive(false);
+        EndlessSelectUI.SetActive(true);
     }
 
 }
