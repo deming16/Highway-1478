65345147e400be92e5d9c5d0c93dbd8bc8b9e072
Lin Deming <neverggyay@gmail.com>
2018-07-02 11:49:09 +0800

    Add Quitgame save score to Dungeon/skytrain

--- Assets/MainMenu/Scripts_MainMenu/PauseMenu.cs
+++ Assets/MainMenu/Scripts_MainMenu/PauseMenu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
 
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI;
-	
+    public Text score;
+
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -39,6 +41,13 @@ public class PauseMenu : MonoBehaviour {
 
     public void QuitGame()
     {
+        string level = SceneManager.GetActiveScene().name.Substring(8);
+
+        if (PlayerPrefs.GetInt(level + "Score", 0) < int.Parse(score.text))
+        {
+            PlayerPrefs.SetInt(level + "Score", int.Parse(score.text));
+        }
+
         Destroy(GameObject.Find("AudioManager"));
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
