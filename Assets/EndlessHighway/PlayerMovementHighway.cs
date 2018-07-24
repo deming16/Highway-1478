bc0e3889ed4186d8c669863e5d071b0235f65bac
LAPTOP-64NSP66G\bryan <bryancgh@hotmail.com>
2018-07-24 17:41:55 +0800

    added test feature(dash move)

--- Assets/EndlessHighway/PlayerMovementHighway.cs
+++ Assets/EndlessHighway/PlayerMovementHighway.cs
@@ -8,8 +8,7 @@ public class PlayerMovementHighway : MonoBehaviour {
 
     public LayerMask Ground;
     public Rigidbody rb;
-    public float fallMultiplier = 2.5f;
-    public float lowMultiplier = 2f;
+    public float dashSpeed = 20;
 
 
     private Vector3 _velocity;
@@ -39,8 +38,10 @@ public class PlayerMovementHighway : MonoBehaviour {
             }
         }
         transform.position += Vector3.forward * Time.deltaTime * forwardSpeed;
-        //rb.AddForce(Vector3.forward * forwardSpeed);
-        if (forwardSpeed <= 40f)
+        if (Input.GetKey(KeyCode.W)) { 
+        rb.AddForce(Vector3.forward * forwardSpeed * dashSpeed);
+    }
+            if (forwardSpeed <= 40f)
         {
 
             forwardSpeed += 0.1f * Time.deltaTime;
